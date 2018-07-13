b63866d2efb058527a8aaeb76edbdede29b5df71
Amy San Felipe <amysanfelipe@gmail.com>
2018-07-13 11:25:39 -0400

    setup server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const parser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
