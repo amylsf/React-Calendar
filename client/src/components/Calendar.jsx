c4c594d20dd002c67483d176587fd54035a41d2f
Amy San Felipe <amysanfelipe@gmail.com>
2018-07-13 12:37:20 -0400

    renders first day of each week for current month

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import Week from './Week.jsx';
 
 class Calendar extends Component {
   constructor() {
@@ -11,6 +12,7 @@ class Calendar extends Component {
     this.getNextMonth = this.getNextMonth.bind(this);
     this.getPreviousMonth = this.getPreviousMonth.bind(this);
     this.getCurrentMonth = this.getCurrentMonth.bind(this);
+    this.displayWeeks = this.displayWeeks.bind(this);
   }
 
   getNextMonth() {
@@ -31,13 +33,41 @@ class Calendar extends Component {
     })
   }
 
+  displayWeeks() {
+    let weeks = [];
+    let date = this.state.month.clone().startOf('month').day(0); //get date of the sunday of the first week of a month
+    let generatedWeeks = false;
+    let weekCount = 0;
+    let currentMonth = date.month();
+    
+    while (!generatedWeeks) {
+      weeks.push(date.clone().format('dd')); //[TO DO] add week component in place of this!!
+      date.add(1, 'week'); //gets each sunday of the month
+      weekCount++;
+      generatedWeeks = weekCount > 1 && currentMonth !== date.month(); //stops generating new weeks when sunday falls in new month
+    }
+    return weeks;
+  }
+
   render() {
     return (
       <div>
-        <div className="month">{this.state.month.format('MMMM YYYY')}</div>
-        <button onClick={this.getPreviousMonth}>Previous</button>
-        <button onClick={this.getCurrentMonth}>Today</button>
-        <button onClick={this.getNextMonth}>Next</button>
+        <div>
+          <div className="month">{this.state.month.format('MMMM YYYY')}</div>
+          <button onClick={this.getPreviousMonth}>Previous</button>
+          <button onClick={this.getCurrentMonth}>Today</button>
+          <button onClick={this.getNextMonth}>Next</button>
+        </div>
+        <div className="day-names">
+          <span className="day-name">Sun</span>
+          <span className="day-name">Mon</span>
+          <span className="day-name">Tue</span>
+          <span className="day-name">Wed</span>
+          <span className="day-name">Thu</span>
+          <span className="day-name">Fri</span>
+          <span className="day-name">Sat</span>
+        </div>
+        {this.displayWeeks()}
       </div>
     )
   }
