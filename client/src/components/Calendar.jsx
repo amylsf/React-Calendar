e87a2f9325a834461a5f8583c902e137ef51f0b1
Amy San Felipe <amysanfelipe@gmail.com>
2018-07-13 12:02:46 -0400

    header renders current month. months switch upon button click

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -5,13 +5,40 @@ class Calendar extends Component {
   constructor() {
     super()
     this.state = {
-
+      month: moment()
     }
+
+    this.getNextMonth = this.getNextMonth.bind(this);
+    this.getPreviousMonth = this.getPreviousMonth.bind(this);
+    this.getCurrentMonth = this.getCurrentMonth.bind(this);
+  }
+
+  getNextMonth() {
+    this.setState({
+      month: this.state.month.add(1, 'month')
+    })
+  }
+
+  getPreviousMonth() {
+    this.setState({
+      month: this.state.month.subtract(1, 'month')
+    })
+  }
+
+  getCurrentMonth() {
+    this.setState({
+      month: moment()
+    })
   }
 
   render() {
     return (
-      <div></div>
+      <div>
+        <div className="month">{this.state.month.format('MMMM YYYY')}</div>
+        <button onClick={this.getPreviousMonth}>Previous</button>
+        <button onClick={this.getCurrentMonth}>Today</button>
+        <button onClick={this.getNextMonth}>Next</button>
+      </div>
     )
   }
 }
